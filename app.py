08f32a0406fa05c15e57fa039948b72035441e93
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-03-05 21:46:20 +0000

    request payment to download tiff

--- app.py
+++ app.py
@@ -15,11 +15,17 @@ from werkzeug.utils import secure_filename
 import subprocess
 from dotenv import load_dotenv
 from tasks import background_task
-from emails import send_email, send_notification_upload_email, send_email_to_admin
+from emails import (
+    send_email,
+    send_notification_upload_email,
+    send_email_to_admin,
+)  # noqa: E501
+import stripe
 
 load_dotenv()
 
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
 ALLOWED_EXTENSIONS_TIFF = {".tiff", ".tif"}
 ALLOWED_EXTENSIONS_ECW = {".ecw"}
 
@@ -37,6 +43,7 @@ def allowed_ecw_file_extension(filename):
 
 def allowed_tiff_file_extension(filename):
     split = os.path.splitext(filename)
+    session["paid"] = True
     file_extension = split[1].lower()
     return file_extension in ALLOWED_EXTENSIONS_TIFF
 
@@ -69,7 +76,10 @@ def upload_file():
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
         filename_hash = hashlib.md5(filename.encode("utf-8")).hexdigest()
-        session["file"] = {"filename_hash": filename_hash, "filename": filename}
+        session["file"] = {
+            "filename_hash": filename_hash,
+            "filename": filename,
+        }  # noqa: E501
         # Store filename_hash -> filename in the current session
         send_notification_upload_email()
         return redirect(
@@ -105,9 +115,54 @@ def upload_complete(filename, email):
 
 @app.route("/download/<filename>", methods=["GET"])
 def download_file(filename):
+    session["has_paid"] = True
     new_name = filename.rsplit(".", 1)
     filename = new_name[0] + "_converted.tiff"
     return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+@app.route("/cancel-confirm/<filename>", methods=["GET"])
+def cancel_confirm(filename):
+    return """
+    <h1>Are you sure you want to cancel?</h1>
+    Your .tiff file cannot be downloaded without payment.
+
+    <a href="{}">Proceed to payment</a>.
+    """.format(
+        url_for("payment", filename=filename)
+    )
+
+
+@app.route("/payment-complete/<filename>", methods=["GET"])
+def payment_complete(filename):
+    return redirect(url_for("download_file", filename=filename))
+
+
+@app.route("/payment/<filename>", methods=["GET"])
+def payment(filename):
+    stripe.api_key = STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "USD",
+                    "product_data": {
+                        "name": "Your tiff download is ready",  # noqa: E501
+                        "description": f"{filename.replace('.ecw', '.tiff')}",
+                    },
+                    "unit_amount": 50,
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=f"{url_for('download_file', filename=filename, _external=True)}",  # noqa: E501
+        cancel_url=url_for(
+            "cancel_confirm", filename=filename, _external=True
+        ),  # noqa: E501
+    )
+    return redirect(checkout_session.url, code=303)
 
 
 @background_task
--- emails.py
+++ emails.py
@@ -16,15 +16,17 @@ def send_email(email=None, filename=None):
     message["Subject"] = "Your MAP conversion is ready"
     message["From"] = sender_email
     message["To"] = receiver_email
-    link = f"""\
+    body = f"""\
     <html>
         <body>
             <p> Please use this link to </p>
-            <a href="http://{domain}/download/{filename}"> download file </a>
+            <a href="http://{domain}/payment/{filename}"> download file </a>
         </body>
     </html>
     """
-    part1 = MIMEText(link, "html")
+    print(f"Sending email to {email} with body:")
+    print(body)
+    part1 = MIMEText(body, "html")
     message.attach(part1)
     context = ssl.create_default_context()
     with smtplib.SMTP(smtp_server, port) as server:
@@ -68,31 +70,34 @@ def send_email_to_admin(email=None, filename=None):
 
 
 def send_notification_upload_email():
-    port = os.getenv("PORT")
-    sender_email = os.getenv("SENDER_EMAIL")
-    smtp_server = os.getenv("SMTP_SERVER")
-    password = os.getenv("PASSWORD")
-    receiver_email = os.getenv("receiver_email_1")
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "A new map has been uploaded"
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    link = f"""\
-    <html>
-        <body>
-            <p> A newmap has been uploaded!! </p>
-        </body>
-    </html>
-    """
-    part1 = MIMEText(link, "html")
-    message.attach(part1)
-    context = ssl.create_default_context()
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.starttls(context=context)
-        server.login(sender_email, password)
-        server.sendmail(
-            sender_email,
-            receiver_email,
-            message.as_string(),
-        )
-    print("email sent")
+    try:
+        port = os.getenv("PORT")
+        sender_email = os.getenv("SENDER_EMAIL")
+        smtp_server = os.getenv("SMTP_SERVER")
+        password = os.getenv("PASSWORD")
+        receiver_email = os.getenv("receiver_email_1")
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "A new map has been uploaded"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        link = """\
+        <html>
+            <body>
+                <p> A newmap has been uploaded!! </p>
+            </body>
+        </html>
+        """
+        part1 = MIMEText(link, "html")
+        message.attach(part1)
+        context = ssl.create_default_context()
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.login(sender_email, password)
+            server.sendmail(
+                sender_email,
+                receiver_email,
+                message.as_string(),
+            )
+        print("email sent")
+    except Exception as e:
+        print(f"Error sending send_notification_upload_email: {e}")
